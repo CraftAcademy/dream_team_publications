9b3dc3c5477322fe7a0c3921f782fc7be2cf9252
Holger_Tidemand <tidemand.holger@gmail.com>
2018-03-14 16:50:31 +0100

    adds form for creating articles adds create function in controller adds display sucess flash when article is created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,22 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = 'Article successfully created'
+      redirect_to article_path(@article)
+    else
+
+    end
+  end
+
   def show
+  end
+
+  private
+
+  def article_params
+    params.permit(:title, :body)
   end
 end
