1e8effd26bbf5b5fb72c79e197a406763ef28200
Lara Thordardottir <lara-thordar@hotmail.com>
2018-03-15 19:15:42 +0100

    fix according to pr review

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root controller: :index, action: :index
   devise_for :users
   root controller: :index, action: :index
   resources :articles, only: [:new, :create, :show, :destroy]
