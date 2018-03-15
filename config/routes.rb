ca5b0871cb16b5a00c30c70806c6d4b853905d15
FleurTamboezer <ftamboezer@gmail.com>
2018-03-15 17:05:06 +0100

    removed double line in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   root controller: :index, action: :index
   resources :articles, only: [:new, :create, :show, :destroy]
   devise_for :users
-  root controller: :index, action: :index
 end
