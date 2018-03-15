7fcc93956a725344ceff33de733b8056b3417ef3
FleurTamboezer <ftamboezer@gmail.com>
2018-03-15 17:03:39 +0100

    removed login_steps.rb due to double coding

--- features/step_definitions/login_steps.rb
+++
@@ -1,5 +0,0 @@
-Given("following user exist") do |table|
-  table.hashes.each do |user|
-    FactoryBot.create(:user, user)
-  end
-end
--- features/step_definitions/register_steps.rb
+++ features/step_definitions/register_steps.rb
@@ -2,8 +2,8 @@ Then("I should be redirected to {string} Page") do |page_name|
   expect(page.current_path).to eq page_path(page_name)
 end
 
-Given('the following user exist') do |table|
+Given('following user exist') do |table|
   table.hashes.each do |user|
-    FactoryBot.create(:user, user)
+    create(:user, user)
   end
 end
