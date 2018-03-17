42f37eb58b32eb2b154c5c2260e536c2838d272a
FleurTamboezer <ftamboezer@gmail.com>
2018-03-17 12:26:44 +0100

    migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_15_192603) do
+ActiveRecord::Schema.define(version: 2018_03_17_110854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 2018_03_15_192603) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "articles_categories", id: false, force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "article_id", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "subject"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
