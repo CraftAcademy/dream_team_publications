153b3c22a4a5228875eaeaf41e2b958a78dab744
Holger_Tidemand <tidemand.holger@gmail.com>
2018-03-18 13:54:16 +0100

    removes API_Key from VCS

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,15 @@ module Dreamteam
       generate.controller_specs false
       generate.system_tests false
     end
+
+    config_files = ['secret.yml']
+
+    config_files.each do |file_name|
+      file_path = File.join(Rails.root, 'config', file_name)
+      config_keys = HashWithIndifferentAccess.new(YAML::load(IO.read(file_path)))[Rails.env]
+      config_keys.each do |k,v|
+        ENV[k.upcase] ||= v
+      end
+    end
   end
 end
