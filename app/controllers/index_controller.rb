f98a4b7ad2b3001caa92a0e55d3e0d1f97512f95
Holger_Tidemand <tidemand.holger@gmail.com>
2018-03-19 17:14:06 +0100

    removed image_tag from index.html

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,7 @@
 require 'news-api'
 class IndexController < ApplicationController
   def index
-    api_key = News.new(ENV['SECRET_KEY_NEWS_API'])
+    api_key = News.new(Rails.application.credentials.news[:news_api_key])
     @api_news_feed = api_key.get_top_headlines(category: 'technology', country: 'us', pageSize: 5)
   end
 end
