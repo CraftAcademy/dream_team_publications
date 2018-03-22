5c2c313883502024360304721828f0d5836f1dee
bollen <david.boll@me.com>
2018-03-22 15:08:11 +0100

    Adds stripe-keys to credentials

--- db/schema.rb
+++ db/schema.rb
@@ -77,9 +77,10 @@ ActiveRecord::Schema.define(version: 2018_03_22_084723) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "subscriber", default: false
+    t.string "role", default: "visitor"
     t.float "latitude"
     t.float "longitude"
+    t.boolean "subscriber", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
