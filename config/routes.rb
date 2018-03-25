96a696d4373322605274daeb2dbacfb505aeba34
kalle <kmthorsnes@gmail.com>
2018-03-25 22:02:24 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,27 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /en|sv/ do
-    root controller: :index, action: :index
-    resources :articles, shallow: true do
-      # resources :comments, only: %i[new create show]
-      resources :comments, only: [:new, :create, :show]
-    end
-    resources :categories, only: [:show]
-    devise_for :users
-    resources :subscriptions, only: [:new, :create]
+  root controller: :index, action: :index
+  resources :articles, shallow: true do
+    resources :comments, only: [:new, :create, :show]
   end
+  resources :categories, only: [:show]
+  devise_for :users
+  resources :subscriptions, only: [:new, :create]
 end
+end
+
+# Rails.application.routes.draw do
+#   scope '(:locale)', locale: /en|sv/ do
+#     root controller: :index, action: :index
+#     resources :articles, shallow: true do
+#       # resources :comments, only: %i[new create show]
+#       resources :comments, only: [:new, :create, :show]
+#     end
+#     resources :categories, only: [:show]
+#     devise_for :users
+#     resources :subscriptions, only: [:new, :create]
+#   end
+# end
 
 
 # Rails.application.routes.draw do
