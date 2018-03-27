65bca3646693653aac64b28698369dfdda2ddd55
kalle <kmthorsnes@gmail.com>
2018-03-27 21:53:21 +0200

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root controller: :index, action: :index
   resources :index, only: [:create, :index]
+  namespace :api do
+    resources :articles
+end
   resources :articles, shallow: true do
     resources :comments, only: [:new, :create, :show]
   end
