1b97cae506d9a4cbd36f5596d381bcb9f4fcc121
kalle <kmthorsnes@gmail.com>
2018-03-28 10:34:44 +0200

    add index method in article controller pass article spec test

--- app/controllers/api/v0/articles_controller.rb
+++ app/controllers/api/v0/articles_controller.rb
@@ -1,3 +1,5 @@
 class Api::V0::ArticlesController < ApplicationController
-
+  def index
+    render json: { message: 'Pong' }
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   root controller: :index, action: :index
-  resources :index, only: [:create, :index]
+  resources :index, only: %i[create index]
   namespace :api do
-  namespace :v0 do
-  resources :articles, only: [:index], constraints: { format: 'json' }
+    namespace :v0 do
+      resources :articles, only: [:index], constraints: { format: 'json' }
+    end
   end
-end
   resources :articles, shallow: true do
-    resources :comments, only: [:new, :create, :show]
+    resources :comments, only: %i[new create show]
   end
   resources :categories, only: [:show]
   devise_for :users
-  resources :subscriptions, only: [:new, :create]
+  resources :subscriptions, only: %i[new create]
 end
